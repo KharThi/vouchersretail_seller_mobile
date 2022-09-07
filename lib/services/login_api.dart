6b98262b5fb12dc124376bb6766ff7150871f90f
KharThi <62747529+KharThi@users.noreply.github.com>
2022-09-07 10:56:06 +0700

    Init

---
+++ lib/services/login_api.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:nyoba/constant/constants.dart';
+import 'package:nyoba/constant/global_url.dart';
+import 'package:nyoba/services/session.dart';
+import 'package:nyoba/utils/utility.dart';
+import 'package:http/http.dart' as http;
+
+class LoginAPI {
+  loginByDefault(String? username, String? password) async {
+    Map data = {'email': username, 'passwrod': password};
+    // var response = await baseAPI.postAsync(
+    //   '$loginDefault',
+    //   data,
+    //   isCustom: true,
+    // );
+    var body = json.encode(data);
+    var response2 = await http.post(
+        Uri.parse("https://webapp-220831200534.azurewebsites.net/api/v1/login"),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body);
+    print("object");
+    // return response;
+
+    // var dataResponse = await json.decode(response2.body);
+    print("Bearer " + response2.body);
+    // print("Response 1" + dataResponse.toString());
+    var response = await http.get(
+        Uri.parse(
+            "https://webapp-220831200534.azurewebsites.net/api/v1/users/current"),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + response2.body
+        });
+    var dataResponse2 = await json.decode(response.body);
+    print("Response 2" + dataResponse2.toString());
+    return dataResponse2;
+  }
+
+  loginByOTP(phone) async {
+    var response =
+        await baseAPI.getAsync('$signInOTP?phone=$phone', isCustom: true);
+    return response;
+  }
+
+  loginByGoogle(token) async {
+    var response = await baseAPI.getAsync('$signInGoogle?access_token=$token',
+        isCustom: true);
+    return response;
+  }
+
+  loginByFacebook(token) async {
+    var response = await baseAPI.getAsync('$signInFacebook?access_token=$token',
+        isCustom: true);
+    return response;
+  }
+
+  loginByApple(email, displayName, userName) async {
+    Map data = {
+      'email': email,
+      'display_name': displayName,
+      'user_name': userName
+    };
+    var response = await baseAPI.postAsync('$signInApple', data,
+        isCustom: true, printedLog: true);
+    return response;
+  }
+
+  inputTokenAPI() async {
+    Map data = {
+      'token': Session.data.getString('device_token'),
+      'cookie': Session.data.getString('cookie')
+    };
+    printLog(data.toString(), name: 'Token Firebase');
+    var response = await baseAPI.postAsync(
+      '$inputTokenUrl',
+      data,
+      isCustom: true,
+    );
+    return response;
+  }
+
+  forgotPasswordAPI(String? email) async {
+    Map data = {'email': email};
+    var response = await baseAPI.postAsync(
+      '$forgotPasswordUrl',
+      data,
+      isCustom: true,
+    );
+    return response;
+  }
+}
---
+++ lib/services/register_api.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:nyoba/constant/constants.dart';
+import 'package:nyoba/constant/global_url.dart';
+import 'package:http/http.dart' as http;
+
+class RegisterAPI {
+  register(String? firstName, String? lastName, String? email, String? username,
+      String? password, String phone) async {
+    Map data = {
+      // "user_email": email,
+      // "user_login": username,
+      "userName": username,
+      "password": password,
+      "email": email,
+      "role": "Admin",
+      // "last_name": lastName,
+      "phoneNumber": phone
+    };
+    // var response2 = await baseAPI2.postAsync(
+    //   '$signUp2',
+    //   data,
+    //   isCustom: true,
+    // );
+    var body = json.encode(data);
+    var response = await http.post(
+        Uri.parse(
+            "https://webapp-220831200534.azurewebsites.net/api/v1/login/signup"),
+        headers: {"Content-Type": "application/json"},
+        body: body);
+    print(body);
+    var dataResponse = await json.decode(response.body);
+    print("Response" + dataResponse.toString());
+    return dataResponse;
+  }
+}
